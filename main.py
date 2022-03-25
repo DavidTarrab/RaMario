01d38d2c1915707e8a31e2fadd973875ace8ffbf
DavidTarrab <71798870+DavidTarrab@users.noreply.github.com>
2022-03-25 13:20:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ vec = pygame.math.Vector2  # 2 for two dimensional
 HEIGHT = 450
 WIDTH = 400
 FPS = 60
-GRAV = .8
+GRAV = 1.2
  
 FramePerSec = pygame.time.Clock()
  
@@ -27,38 +27,80 @@ class Player(pygame.sprite.Sprite):
 		self.vel = vec(0,0)
 		self.acc = vec(0,0)
 
+		self.facingRight = True
+
 	def walkRight(self):
-		self.acc.x = 1.1
-		if self.vel.x >= 10:
-			self.vel.x = 10
+		self.acc.x = 0.8
+		self.facingRight = True
 
 	def walkLeft(self):
-		self.acc.x = -1.1
-		if self.vel.x <= -10:
-			self.vel.x = -10
+		self.acc.x = -0.8
+		self.facingRight = False
 	
 	def update(self):
+		self.acc = vec(0, GRAV)
+
+		# Get key presses
 		pressed_keys = pygame.key.get_pressed()
+		# Trigger movement with key presses
 		if pressed_keys[K_RIGHT]:
 			self.walkRight()
-		if pressed_keys[K_LEFT]:
+		elif pressed_keys[K_LEFT]:
 			self.walkLeft()
+		# Deceleration on right facing
+		elif self.facingRight:
+			if self.vel.x > 0:
+				self.acc.x = -0.4
+			elif self.vel.x < 0:
+				self.vel.x = 0
+				self.acc.x = 0
+		# Deceleration on left facing
+		elif not self.facingRight:
+			if self.vel.x < 0:
+				self.acc.x = 0.4
+			elif self.vel.x > 0:
+				self.vel.x = 0
+				self.acc.x = 0
+
+		# Update the velocity and position using kinematics
 		self.vel += self.acc
 		self.pos += self.vel + 0.5 * self.acc
-		self.rect.midbottom = self.pos
 
+		# Block left screen
+		if self.pos.x < 0:
+			self.pos.x = 0
+
+		# Speed cap
+		if self.vel.x >= 8:
+			self.vel.x = 8
+		if self.vel.x <= -8:
+			self.vel.x = -8
+
+		# Scrolling
+		if self.rect.right >= WIDTH / 3:
+			self.pos.x -= (abs(self.vel.x) + abs(0.5*self.acc.x))
+			for plat in platforms:
+				plat.rect.x -= abs(self.vel.x)
+				if plat.rect.right <= 0:
+					plat.kill()
+
+		# Collision detection
 		hits = pygame.sprite.spritecollide(P1 , platforms, False)
 		if hits:
 			if P1.vel.y > 0:
 				self.pos.y = hits[0].rect.top + 1
-			if P1.vel.y < 0:
+				self.vel.y = 0
+			if P1.vel.y < 0 and not self.pos.y < hits[0].rect.top:
 				self.pos.y = hits[0].rect.bottom + 30
-			self.vel.y = 0
+				self.vel.y = 0
+
+		# Update actual position of the sprite
+		self.rect.bottomleft = self.pos
 
 	def jump(self):
 		hits = pygame.sprite.spritecollide(self, platforms, False)
 		if hits:
-			self.vel.y = -18
+			self.vel.y = -23
  
 class platform(pygame.sprite.Sprite):
 	def __init__(self, width, height, x, y):
@@ -81,8 +123,8 @@ all_sprites.add(P1)
 
 platforms = pygame.sprite.Group()
 platforms.add(PT1)
-for x in range(random.randint(5, 6)):
-	pl = platform(random.randint(50,100), 12, random.randint(0,WIDTH-10), random.randint(0, HEIGHT-30))
+for x in range(random.randint(5, 10)):
+	pl = platform(random.randint(50,100), 12, random.randint(100,WIDTH*5), random.randint(50, HEIGHT-50))
 	platforms.add(pl)
 	all_sprites.add(pl)
 
@@ -94,18 +136,10 @@ while True:
 		if event.type == pygame.KEYDOWN:    
 			if event.key == pygame.K_SPACE:
 				P1.jump()
-
-	if P1.rect.right >= WIDTH / 3:
-		P1.pos.x -= abs(P1.vel.x)
-		for plat in platforms:
-			plat.rect.x -= abs(P1.vel.x)
-			if plat.rect.right <= 0:
-				plat.kill()
 	 
 	displaysurface.fill((0,0,0))
-
+ 
 	P1.update()
-	print(P1.vel.x)
 	for entity in all_sprites:
 		displaysurface.blit(entity.surf, entity.rect)
 
